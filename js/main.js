f62e494503319f0714c55216d396d388556e03ad
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-02-09 12:55:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -100,3 +100,25 @@ const MAIN_SLIDE_NAV_HANDLER = e => {
     MAIN_VISUAL_SLIDE.slideTo(I + 1);
 }
 MAIN_SLIDE_NAV.addEventListener('click', MAIN_SLIDE_NAV_HANDLER);
+
+const PF_LEFT_SLIDE_OPTION = {
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+    // autoplay: {
+    //     delay: 3000,
+    // },
+    watchSlidesProgress: true,
+}
+const PF_LEFT_SLIDE = new Swiper('.pf_left_slide', PF_LEFT_SLIDE_OPTION);
+
+const PF_RIGHT_SLIDE_OPTION = {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 30,
+}
+const PF_RIGHT_SLIDE = new Swiper('.pf_right_slide', PF_RIGHT_SLIDE_OPTION);
+PF_LEFT_SLIDE.controller.control = PF_RIGHT_SLIDE;
+PF_RIGHT_SLIDE.controller.control = PF_LEFT_SLIDE;
