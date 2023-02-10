e7dacfe339dd869c367ab970b132a3426c91b287
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-02-10 12:49:12 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -102,23 +102,59 @@ const MAIN_SLIDE_NAV_HANDLER = e => {
 MAIN_SLIDE_NAV.addEventListener('click', MAIN_SLIDE_NAV_HANDLER);
 
 const PF_LEFT_SLIDE_OPTION = {
-    loop: true,
+    // loop: true,
     effect: 'fade',
     fadeEffect: {
         crossFade: true,
     },
+    navigation: {
+        nextEl: "#mainPortfolio .arrows li:nth-child(2)",
+        prevEl: "#mainPortfolio .arrows li:nth-child(1)",
+    }
     // autoplay: {
     //     delay: 3000,
     // },
-    watchSlidesProgress: true,
 }
 const PF_LEFT_SLIDE = new Swiper('.pf_left_slide', PF_LEFT_SLIDE_OPTION);
 
 const PF_RIGHT_SLIDE_OPTION = {
-    loop: true,
+    // loop: true,
     slidesPerView: 5,
     spaceBetween: 30,
 }
 const PF_RIGHT_SLIDE = new Swiper('.pf_right_slide', PF_RIGHT_SLIDE_OPTION);
 PF_LEFT_SLIDE.controller.control = PF_RIGHT_SLIDE;
 PF_RIGHT_SLIDE.controller.control = PF_LEFT_SLIDE;
+
+const MS_CONTENT = document.querySelectorAll('#mainSolution .Ms_content .content');
+const MS_NUM = document.querySelector('#mainSolution .base .num')
+const MS_SLIDE_OPTION = {
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 100,
+    centeredSlides: true,
+    slideActiveClass: "on",
+    on: {
+        slideChangeTransitionStart: function () {
+            console.log(this.realIndex, this.slides.length);
+            let idx = this.realIndex;
+            let total = MS_CONTENT.length;
+            // for (let i = 0; i < MS_CONTENT.length; i++) {
+            //     MS_CONTENT[i].classList.remove('on');
+            // }
+
+            MS_NUM.innerHTML = `<strong>${idx < 9 ? 0 : ''}${idx + 1}</strong> / <span>${idx < 9 ? 0 : ''}${total}</span>`;
+            MS_CONTENT.forEach(it => it.classList.remove('on'));
+            MS_CONTENT[idx].classList.add('on');
+        }
+    },
+    navigation: {
+        nextEl: "#mainSolution .arrows li:nth-child(2)",
+        prevEl: "#mainSolution .arrows li:nth-child(1)",
+    },
+    pagination: {
+        el: '#mainSolution .dots',
+        clickable: true,
+    }
+}
+const MS_SLIDE = new Swiper('.Ms_slide', MS_SLIDE_OPTION);
